b8cad3c35271f8b45996df96704715bc8f80759a
Pawar-Darshan <officialdarshan007@gmail.com>
2022-05-14 20:36:07 +0530

    Add To Cart

---
+++ src/data.js
@@ -0,0 +1,55 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+import img4 from './img/img4.png';
+import img5 from './img/img5.png';
+import img6 from './img/img6.png';
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: "Spicy Hamburgers",
+            desc: "",
+            price: 46,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: "Cheezy Pizza",
+            desc: "",
+            price: 56,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: "Cake",
+            desc: "",
+            price: 46,
+        },
+        {
+            id: 4,
+            img: img4,
+            title: "Mexican Tacos",
+            desc: "",
+            price: 32,
+        },
+        {
+            id: 5,
+            img: img5,
+            title: "Milk Shake",
+            desc: "",
+            price: 36,
+        },
+        {
+            id: 6,
+            img: img6,
+            title: "Doughnuts",
+            desc: "",
+            price: 66,
+        }
+    ],
+};
+
+export default data;
